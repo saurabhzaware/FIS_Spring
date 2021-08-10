902d5e51484f57a8078f5319f927f3f0d117ab28
saurabhzaware <saurabhzaware1199@gmail.com>
2021-08-10 15:56:12 +0530

    Adding multiple beans

---
+++ spring-learn/src/main/java/com/fis/springlearn/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.fis.springlearn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import com.fis.springlearn.services.EmployeeService;
+
+@Component
+@Configuration
+@ComponentScan("com.fis.springlearn.bean")
+public class EmployeeController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
+
+	private EmployeeService employeeService;
+
+	public EmployeeController() {
+		LOGGER.debug("Inside Employee Controller");
+	}
+
+	@Autowired
+	public void setEmployeeService(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+}
---
+++ spring-learn/src/main/java/com/fis/springlearn/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.fis.springlearn.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fis.springlearn.dao.EmployeeDao;
+
+@Component
+public class EmployeeService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
+
+	private EmployeeDao employeeDao;
+
+	public EmployeeService() {
+		LOGGER.debug("Inside Employee Service");
+	}
+
+	@Autowired
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+}
