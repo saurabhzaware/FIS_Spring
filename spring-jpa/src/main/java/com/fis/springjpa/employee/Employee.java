b17a40ceb6f89bbf94e42a29b10372f7d3535619
saurabhzaware <saurabhzaware1199@gmail.com>
2021-08-11 18:14:38 +0530

    Added Employee using Annotations

---
+++ spring-jpa/src/main/java/com/fis/springjpa/employee/Employee.java
@@ -0,0 +1,35 @@
+package com.fis.springjpa.employee;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Employee {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  //private double salary;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+	
+}
---
+++ spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.fis.springjpa.employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
