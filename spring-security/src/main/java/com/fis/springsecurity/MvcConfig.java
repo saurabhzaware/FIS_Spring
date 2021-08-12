8a86e66f30ac94720ab6afdba46c341cf913c75d
saurabhzaware <saurabhzaware1199@gmail.com>
2021-08-12 15:36:28 +0530

    Added JPA Product with security

--- spring-security/src/main/java/com/fis/springsecurity/MvcConfig.java
+++ spring-security/src/main/java/com/fis/springsecurity/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.fis.springsecurity;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
